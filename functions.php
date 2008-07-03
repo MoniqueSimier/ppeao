5e324dc49a0175b89beefc1b05b0af2262b2afbc
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2008-07-03 06:50:13 +0000

    on repart de zéro - importation
    git-svn-id: https://svn.mpl.ird.fr/ppeao@8 516e780c-6b34-0410-bf16-dade293637d1

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?
+// LIST OF PHP FILES CONTAINING FUNCTIONS USED APPLICATION-WIDE
+
+// generic functions
+include 'functions_generic.php';
+
+// specific functions for ppeao
+include 'functions_ppeao.php';
+
+
+?>
