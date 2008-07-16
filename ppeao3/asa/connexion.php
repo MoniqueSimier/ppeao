bd31580a931ac33cb7a3b17598eb73754b86917d
granouil <granouil@516e780c-6b34-0410-bf16-dade293637d1>
2008-07-16 12:14:48 +0000

    maj fichier connexion (serveur distant vmppeao.mpl.ird.fr)
    git-svn-id: https://svn.mpl.ird.fr/ppeao@63 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/asa/connexion.php
+++ ppeao3/asa/connexion.php
@@ -1,9 +1,10 @@
 <?php
 $user="devppeao";			// Le nom d'utilisateur 
 $passwd="2devppe!!";			// Le mot de passe 
-//$host= "vmppeao.mpl.ird.fr";	// L'hôte (ordinateur sur lequel le SGBD est installé) 
-$host= "localhost";	// L'hôte (ordinateur sur lequel le SGBD est installé) 
-if($bdd=="") $bdd= "bd_peche";
+$host= "vmppeao.mpl.ird.fr";	// L'hôte (ordinateur sur lequel le SGBD est installé) 
+//$host= "localhost";	// L'hôte (ordinateur sur lequel le SGBD est installé) 
+//if($bdd=="") $bdd= "bd_peche";
+if($bdd=="") $bdd= "peche_exp_27_09";
 
 $connection = pg_connect ("host=".$host." dbname=".$bdd." user=".$user." password=".$passwd);
 if (!$connection) { echo "Pas de connection"; exit;}
