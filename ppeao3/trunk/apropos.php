d54d212d44d771dd5db9a055948a12e04ca672b1
laurenya <laurenya@516e780c-6b34-0410-bf16-dade293637d1>
2008-10-22 07:12:07 +0000

    Ajout des pages contact et a propos
    git-svn-id: https://svn.mpl.ird.fr/ppeao@239 516e780c-6b34-0410-bf16-dade293637d1

---
+++ ppeao3/trunk/apropos.php
@@ -0,0 +1,69 @@
+<?php 
+// Mis à jour par Olivier ROUX, 29-07-2008
+// definit a quelle section appartient la page
+$section="apropos";
+// code commun à toutes les pages (demarrage de session, doctype etc.)
+include $_SERVER["DOCUMENT_ROOT"].'/top.inc';
+
+$zone=0; // zone publique (voir table admin_zones)
+?>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+	
+	<?php 
+		// les balises head communes  toutes les pages
+		include $_SERVER["DOCUMENT_ROOT"].'/head.inc';
+	?>
+	<title>ppeao::&agrave; propos de PPEAO</title>
+
+	
+</head>
+
+<body>
+
+	<?php 
+	// le menu horizontal
+	include $_SERVER["DOCUMENT_ROOT"].'/top_nav.inc';
+
+
+	if (isset($_SESSION['s_ppeao_user_id'])){ // a implementer partout + deploiement de loginform_s.php et function_ppeao.php
+		$userID = $_SESSION['s_ppeao_user_id'];
+	} else {
+		$userID=null;
+	}
+	
+	// on teste à quelle zone l'utilisateur a accès
+	if (userHasAccess($userID,$zone)) {
+	?>
+
+	<div id="main_container" class="home">
+	<div id="main_contact">
+	<h1 class="contact">Peuplement de poissons et la p&ecirc;che artisanale des Ecosyst&egrave;mes estuariens, lagunaires ou continentaux d'Afrique de l'Ouest</h1><br/>
+	<p class="contact">La base PPEAO archive des informations sur les poissons, leur &eacute;cologie et leur exploitation par la p&ecirc;che artisanale de nombreux &eacute;cosyst&egrave;mes aquatiques de 8 pays de l'Afrique de l'Ouest.</p>
+	<p class="contact">Les peuplements de poissons de ces écosytèmes ont été échantillonés soit par des techniques de p&ecirc;che exp&eacute;rimentale, soit par un suivi des p&ecirc;ches artisanales d&eacute;velopp&eacute;es sur ces milieurx. Cette base archive des informations collect&eacute;es &agrave; partir de 1978 et est r&eacute;guli&egrave;rement mise &agrave; jour.
+
+
+	
+	
+		</div>
+	</div> <!-- end div id="main_container"-->
+	
+	
+	<?php 
+	// note : on termine la boucle testant si l'utilisateur a accès à la page demandée
+	
+	;} // end if (userHasAccess($_SESSION['user_id'],$zone))
+	
+	// si l'utilisateur n'a pas accès ou n'est pas connecté, on affiche un message l'invitant à contacter un administrateur pour obtenir l'accès
+	else {userAccessDenied($zone);}
+	?>
+	
+	<?php 
+	include $_SERVER["DOCUMENT_ROOT"].'/footer.inc';
+	
+	?>
+</body>
+</html>
--- ppeao3/trunk/contactRT.php
+++ ppeao3/trunk/contactRT.php
@@ -42,19 +42,17 @@ $zone=0; // zone publique (voir table admin_zones)
 	<div id="main_container" class="home">
 	<div id="main_contact">
 	<h1 class="contact">Peuplement de poissons et la p&ecirc;che artisanale des Ecosyst&egrave;mes estuariens, lagunaires ou continentaux d'Afrique de l'Ouest</h1><br/>
-	<p class="contact">La base PPEAO archive des informations sur les poissons, leur &eacute;cologie et leur exploitation par la p&ecirc;che artisanale de nombreux &eacute;cosyst&egrave;mes aquatiques de l'Afrique de l'Ouest.</p><br/>
-	<p class="contactItalique">Elle a &eacute;t&eacute; con&ccedil;ue et r&eacute;alis&eacute;e par les membres de l'Unit de Recherche RAP (R&eacute;ponses adaptatives des populations et peuplements des poissons aux pressions de l'environnement) de l'IRD (Institut de Recherches pour le D&eacute;veloppement). </p><br/>
-	<p class="contact"><span class="gras">contact</span>
+	<p class="contact">La base PPEAO a &eacute;t&eacute; r&eacute;alis&eacute;e techniquement par: </p>
 	<ul class="contact">
-		<li class="contact"><a href="#" onClick="o='@';o=' 	&#109;&#111;&#110;&#105;&#113;&#117;&#101;&#46;&#115;&#105;&#109;&#105;&#101;&#114;'+o;o='mailto:'+o;o+='ird.fr';this.href=o;"><script type="text/javascript"> <!-- o='@';o='&#109;&#111;&#110;&#105;&#113;&#117;&#101;&#46;&#115;&#105;&#109;&#105;&#101;&#114;'+o;o+='ird.fr';document.write(o);//--> </script>Monique SIMIER</a></li>
-		<li class="contact"><a href="#" onClick="o='@';o='&#106;&#101;&#97;&#110;&#46;&#109;&#97;&#114;&#99;&#46;&#101;&#99;&#111;&#117;&#116;&#105;&#110;'+o;o='mailto:'+o;o+='ird.fr';this.href=o;"><script type="text/javascript"> <!-- o='@';o='&#106;&#101;&#97;&#110;&#46;&#109;&#97;&#114;&#99;&#46;&#101;&#99;&#111;&#117;&#116;&#105;&#110;'+o;o+='ird.fr';document.write(o);//--> </script>Jean-Marc ECOUTIN</a></li>
-	</ul>
-	</p>
-	<p ><a href="/ContactRT.php" >R&eacute;alisation techniques</a>
-	</p><br/>
-	<span class="logo1">IRD</span><span class="logo2">CRH</span><span class="logo3">
-	<UR 070 RAP/span><span class="logo4">PPEAO</span>
-	
+		<li class="contact">SintiGroup</li>
+		<li class="contact">ASA</li>
+		<li class="contact">otolithe</li>
+		</ul>
+	<br/>
+	<p class="contact">R&eacute;f&eacute;rences</p>
+	<p  class="contact"><span class="logo">logo</span><span class="gras">&nbsp;SintiGroup</span><br/> .....</p><br/>
+	<p  class="contact"><span class="logo">logo</span><span class="gras">&nbsp;ASA - Advanced Solutions Accelerator</span><br/>199 rue de l'Oppidum 34170 CASTELNAU LE LEZ<br/>tel : +33 5 67 59 36 40 <br/>cell : +33 06 73 19 70 17<br/>info@advancedsolutionsaccelerator.com</p><br/>
+	<p  class="contact"><span class="logo">logo</span><span class="gras">&nbsp;otolithe</span><br/><a href="http://www.otolithe.com/" target="externe">www.otolithe.com</a><br/>tel : +33 4 67 85 75 88 <br/> cell : +33 6 75 77 12 31<br/><a href="#" onClick="o='@';o='&#111;&#108;&#105;&#118;&#105;&#101;&#114;'+o;o='mailto:'+o;o+='otolithe.fr';this.href=o;"><script type="text/javascript"> <!-- o='@';o=''+o;o+='otolithe.fr';document.write(o);//--> </script>Olivier Roux</a></p>
 	
 		</div>
 	</div> <!-- end div id="main_container"-->
