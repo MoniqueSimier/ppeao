ebf8b0b6b083a827573404316a775c04536db66f
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2008-10-27 08:17:13 +0000

    Erreur de SVN... les fichiers n'ont pas été ajouté...
    git-svn-id: https://svn.mpl.ird.fr/ppeao@257 516e780c-6b34-0410-bf16-dade293637d1

---
+++ ppeao3/trunk/edition_donnees.php
@@ -0,0 +1,80 @@
+<?php 
+// Créé par Olivier ROUX, 02-08-2008
+// code commun à toutes les pages (demarrage de session, doctype etc.)
+include $_SERVER["DOCUMENT_ROOT"].'/top.inc';
+// definit a quelle section appartient la page
+$section="gerer";
+$subsection="donnees";
+
+$zone=2; // zone edition (voir table admin_zones)
+
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+	
+<?php 
+	// les balises head communes  toutes les pages
+	include $_SERVER["DOCUMENT_ROOT"].'/head.inc';
+?>
+	<title>ppeao::g&eacute;rer::tables de donn&eacute;es</title>
+	
+<script src="/js/edition.js" type="text/javascript"  charset="iso-8859-15"></script>
+
+</head>
+
+<body>
+
+<?php 
+// le menu horizontal
+include $_SERVER["DOCUMENT_ROOT"].'/top_nav.inc';
+?>
+
+<div id="main_container" class="edition">
+<h1>g&eacute;rer les tables de donn&eacute;es</h1>
+<!-- édition des tables de référence -->
+<?php
+
+// on teste à quelle zone l'utilisateur a accès
+if (userHasAccess($_SESSION['s_ppeao_user_id'],$zone)) {
+?>
+
+
+<?php
+
+include $_SERVER["DOCUMENT_ROOT"].'/edition/edition_config.inc';
+include $_SERVER["DOCUMENT_ROOT"].'/edition/edition_functions.php';
+
+?>
+
+
+
+<?php
+// on affiche la liste des tables
+echo('<div id="tables_liste">');	
+
+	buildTableList("data");
+
+
+echo('</div>');
+
+?>
+	
+
+<?php 
+// note : on termine la boucle testant si l'utilisateur a accès à la page demandée
+
+;} // end if (userHasAccess($_SESSION['user_id'],$zone))
+
+// si l'utilisateur n'a pas accès ou n'est pas connecté, on affiche un message l'invitant à contacter un administrateur pour obtenir l'accès
+else {userAccessDenied($zone);}
+
+?>
+</div> <!-- end div id="main_container"-->
+<?php 
+include $_SERVER["DOCUMENT_ROOT"].'/footer.inc';
+
+?>
+</body>
+</html>
