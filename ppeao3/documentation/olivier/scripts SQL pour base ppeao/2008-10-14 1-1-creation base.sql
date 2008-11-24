455080e548d2a08524d54f36097765592c9a311b
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2008-11-24 14:15:36 +0000

    Remplacés par de nouveaux scripts
    git-svn-id: https://svn.mpl.ird.fr/ppeao@296 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/documentation/olivier/scripts SQL pour base ppeao/2008-10-14 1-1-creation base.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE ppeao_template
-  WITH OWNER = devppeao
-       ENCODING = 'LATIN9';
-COMMENT ON DATABASE ppeao_template IS 'modèle de base pour l''application PPEAO - contraintes et séquences refaites';
