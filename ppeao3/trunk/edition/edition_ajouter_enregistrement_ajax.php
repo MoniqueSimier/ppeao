db808ad56eae7142a888b87cd3b5a591b6bd421c
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2008-12-19 13:11:00 +0000

    modification de la fonction makeField() : ajout d’un paramètre theTabIndex permettant de définir un “tabindex” pour les éléments de formulaire HTML
    git-svn-id: https://svn.mpl.ird.fr/ppeao@394 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/trunk/edition/edition_ajouter_enregistrement_ajax.php
+++ ppeao3/trunk/edition/edition_ajouter_enregistrement_ajax.php
@@ -30,11 +30,16 @@ $theForm='<h1>Ajouter un nouvel enregistrement dans la table "'.iconv('ISO-8859-
 // on insère le formulaire d'ajout d'un nouvel enregistrement...
 $theForm.='<form id="add_record_'.$level.'_form">';
 $theForm.='<ul id="add_record_'.$level.'_ul">';
+	$tabIndex=0;
 	foreach ($theHeads as $oneHead) {
+		//cas particulier de la table art_stat_effort
+		// on doit saisir un ref_systeme_if OU un ref_secteur_id
+		if ($editTable=='stat_effort' && $oneHead=='ref_systeme_id') {$theForm.='<li class="error small" style="list-style-type: none;">Veuillez saisir un syst&egrave;me OU un secteur.</li>';}
 		$theForm.='<li class="small">';
 		$theForm.='<p>'.$oneHead.': </p>';
-		$theForm.=iconv('ISO-8859-15','UTF-8',makeField($cDetails,$editTable,$oneHead,'','add',''));
-		$theForm.='</li>';	
+		$theForm.=iconv('ISO-8859-15','UTF-8',makeField($cDetails,$editTable,$oneHead,'','add','',$tabIndex));
+		$theForm.='</li>';
+	$tabIndex++;
 	}
 $theForm.='</ul>';
 $theForm.='</form>';
