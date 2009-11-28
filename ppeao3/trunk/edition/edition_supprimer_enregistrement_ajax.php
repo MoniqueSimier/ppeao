543b8e8d5b5f7b7c3cb7b08ba5060698a051731f
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2009-11-28 13:12:24 +0000

    Le contenu de ce script avait été malencontreusement remplacé par celui du script edition_supprimer_enregistrement_validation_ajax.php, ce qui fait que la suppression était faite immédiatement sans confirmation.
    git-svn-id: https://svn.mpl.ird.fr/ppeao@607 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/trunk/edition/edition_supprimer_enregistrement_ajax.php
+++ ppeao3/trunk/edition/edition_supprimer_enregistrement_ajax.php
@@ -7,14 +7,15 @@ include $_SERVER["DOCUMENT_ROOT"].'/connect.inc';
 include $_SERVER["DOCUMENT_ROOT"].'/edition/edition_config.inc';
 include $_SERVER["DOCUMENT_ROOT"].'/functions_generic.php';
 include $_SERVER["DOCUMENT_ROOT"].'/functions_SQL.php';
-include $_SERVER["DOCUMENT_ROOT"].'/functions_ppeao.php';
 include $_SERVER["DOCUMENT_ROOT"].'/edition/edition_functions.php';
-include $_SERVER["DOCUMENT_ROOT"].'/suppression/suppression_functions.php';
+
 
 
 global $tablesDefinitions;
 
 
+
+//debug sleep(10);
 
 // la table concernée
 $table=$_GET["table"];
@@ -32,56 +33,29 @@ $labelArray=pg_fetch_all($labelResult);
 $label=$labelArray[0][$labelColumn];
 pg_free_result($labelResult);
 
-$theMessage.='<div align="center"><h1>supprimer l&#x27;enregistrement &quot;'.$label.'&quot; ('.$key.'="'.$record.'")</h1></div>';
+// on détermine les enregistrements affectés par la suppression de l'enregistrement (on se limite au premier niveau de cascade)
+$affectedTables=countPrimaryKeyReferencedRows($connectPPEAO, $table,$key, $record);
 
-	// on prépare le calcul du nombre total d'enregistrements supprimés
-	// d'abord on calcule le nombre total d'enregistrements dans les tables utilisateur
-	/*// on fait un VACUUM ANALYZE de la base pour être sûr que le compte est correct
-	$vacuumSql='VACUUM ANALYZE';
-	$vacuumResult=pg_query($connectPPEAO,$vacuumSql);
-	pg_free_result($vacuumResult);*/
-	$beforeSql='select sum(n_live_tup) from pg_catalog.pg_stat_user_tables where relname NOT LIKE \'admin_log\'';
-	$beforeResult=pg_query($connectPPEAO,$beforeSql);
-	$beforeArray=pg_fetch_all($beforeResult);
-	$before=$beforeArray[0]["sum"];
-	pg_free_result($beforeResult);
-	
-	$deleteSql='DELETE FROM '.$table.' WHERE '.$key.'=\''.$record.'\';';
-	// si la suppression a bien eu lieu
-	if($deleteResult=pg_query($connectPPEAO,$deleteSql)) {$success='yes';} else {$success='no';}
-	pg_free_result($deleteResult);
-	
-	if ($success=='yes') {
-	// on renvoie un message positif
-	$theMessage.='<p>enregistrement &quot;'.$label.'&quot; ('.$key.'=&quot;'.$record.'&quot;) supprim&eacute; de la table "'.$table.'".</p>';
-	// et on fait un VACUUM ANALYZE de la base
-	$vacuumSql='VACUUM ANALYZE';
-	$vacuumResult=pg_query($connectPPEAO,$vacuumSql);
-	pg_free_result($vacuumResult);
-	
-	// maintenant on recalcule le nombre total d'enregistrements dans les tables utilisateur
-	$afterSql='select sum(n_live_tup) from pg_catalog.pg_stat_user_tables where relname NOT LIKE \'admin_log\'';
-	$afterResult=pg_query($connectPPEAO,$afterSql);
-	$afterArray=pg_fetch_all($afterResult);
-	$after=$afterArray[0]["sum"];
-	pg_free_result($afterResult);
-	
-	// on a donc supprimé $before-$after enregistrements
-	$deletedRows=$before-$after-1;
-	
-	// on le signale si le résultat n'est pas nul ou negatif (bug...)
-	if ($deletedRows>=0) {
-	$theMessage.='<p>'.$deletedRows.' enregistrement(s) d&eacute;pendant(s) supprim&eacute;(s) au total.</p>';}
-	
-	// on inscrit la suppression dans le journal
-	logWriteTo(1,'notice','enregistrement &quot;'.$label.'&quot; ('.$key.'=&quot;'.$record.'&quot;) supprim&eacute; de la table "'.$table.'" et '.$deletedRows.' enregistrement(s) d&eacute;pendant(s) supprim&eacute;(s) au total.',$deleteSql,'',0);
+$theMessage.='<div align="center"><h1 id="delete_title">supprimer l&#x27;enregistrement &quot;'.$label.'&quot; ('.$key.'=&quot;'.$record.'&quot;)</h1></div>';
+
+// si on ne trouve aucun enregistrement affecté
+if (my_empty($affectedTables)) {
+	$theMessage.="<p>Cet enregistrement n&#x27;est pas utilis&eacute; comme cl&eacute; &eacute;trang&egrave;re par d&#x27;autres tables, le supprimer n&#x27;entra&icirc;nera pas de suppression en cascade.</p>";
+}
+
+// si on trouve au moins un enregistrement affecté
+else {
+	$theMessage.="<p>Cet enregistrement est utilis&eacute; comme cl&eacute; &eacute;trang&egrave;re par d&#x27;autres tables, le supprimer entra&icirc;nera la suppression d&#x27;autres enregistrements&nbsp;:</p>";
+	$theMessage.='<ul>';
+	foreach($affectedTables as $key=>$value) {
+		$theMessage.='<li>';
+		$theMessage.='table&nbsp; "'.$key.'" : '.count($value).' enregistrements';
+		$theMessage.='</li>';
 	}
-	
-	else {
-		$theMessage.= '<p>Une erreur est survenue lors de la suppression de l\'enregistrement "'.$record.'" de la table "'.$table.'" :</p><p> '.pg_last_error().'</p>';
-	}
+	$theMessage.='</ul>';
+
+$theMessage.='note : la suppression des enregistrements des tables mentionn&eacute;es ci-dessus pourra entra&icirc;ner &agrave; son tour la suppression d&#x27;autres enregistrements.';
+}
 
 echo($theMessage);
-
-
 ?>
