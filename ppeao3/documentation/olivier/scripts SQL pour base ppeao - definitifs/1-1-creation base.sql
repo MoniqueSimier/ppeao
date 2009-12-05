48a6126b5b40e2192eaa424c8daf3da47a89a46b
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2009-12-05 09:43:20 +0000

    documents temporaires effacés
    git-svn-id: https://svn.mpl.ird.fr/ppeao@633 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/documentation/olivier/scripts SQL pour base ppeao - definitifs/1-1-creation base.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE bdppeao
-  WITH OWNER = devppeao
-       ENCODING = 'LATIN9';
--- ppeao3/documentation/olivier/scripts SQL pour base ppeao - definitifs/1-2-droits base.sql
+++
@@ -1,2 +0,0 @@
-GRANT ALL ON DATABASE ppeao_template TO devppeao;
-GRANT ALL ON DATABASE ppeao_template TO postgres WITH GRANT OPTION;
