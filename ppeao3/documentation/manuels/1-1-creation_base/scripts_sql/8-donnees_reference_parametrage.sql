73bccf74f18afb5620988c0b82c6b3c37ac6338e
rouxo <rouxo@516e780c-6b34-0410-bf16-dade293637d1>
2010-01-15 16:29:19 +0000

    Modification de la valeur "Tous" de la table art_grand_type_engin en "-Tous-" afin qu'elle apparaisse en premier dans l'ordre alphabétique dans les <select>
    git-svn-id: https://svn.mpl.ird.fr/ppeao@699 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/documentation/manuels/1-1-creation_base/scripts_sql/8-donnees_reference_parametrage.sql
+++ ppeao3/documentation/manuels/1-1-creation_base/scripts_sql/8-donnees_reference_parametrage.sql
@@ -338,7 +338,7 @@ INSERT INTO art_grand_type_engin VALUES ('PIEGE', 'Pieges, barrages');
 INSERT INTO art_grand_type_engin VALUES ('SE_PL', 'Senne de plage');
 INSERT INTO art_grand_type_engin VALUES ('SE_SY', 'Senne syndicat');
 INSERT INTO art_grand_type_engin VALUES ('SE_TO', 'Senne tournante');
-INSERT INTO art_grand_type_engin VALUES ('TOUS', 'Tous');
+INSERT INTO art_grand_type_engin VALUES ('TOUS', '-Tous-');
 
 ALTER TABLE art_grand_type_engin ENABLE TRIGGER ALL;
 
