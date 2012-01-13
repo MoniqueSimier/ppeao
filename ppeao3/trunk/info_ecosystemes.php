4c0297914fc7d837f9727ec8485e0ddcc2be6bfc
simier <simier@516e780c-6b34-0410-bf16-dade293637d1>
2012-01-13 10:47:07 +0000

    modification de top_nav.inc pour affichage du logo IRD en haut à droite remplissage du tableau pechart et pechexp dans info_ecosystemes.php MS - 12/01/12
    git-svn-id: https://svn.mpl.ird.fr/ppeao@902 516e780c-6b34-0410-bf16-dade293637d1

--- ppeao3/trunk/info_ecosystemes.php
+++ ppeao3/trunk/info_ecosystemes.php
@@ -40,29 +40,142 @@ $zone=0; // zone publique (voir table admin_zones)
 	?>
 
 	<div id="main_container" class="home">
-	<h1>Projet PPEAO</h1>
+	<h1>PPEAO</h1>
+	<h2 style="text-align:center">Système d'informations sur les Peuplements de poissons et la P&ecirc;che artisanale<br> des Ecosyst&egrave;mes estuariens, lagunaires ou continentaux d&rsquo;Afrique de l&rsquo;Ouest</h2>
+    <br>
 	<h2>P&ecirc;che artisanale</h2>
-	<br>
-<table>
-	<tr>
-		<td>Pays</td>
-		<td>Ecosyst&egrave;me</td>
-		<td>Date d&eacute;but</td>
-		<td>Date fin</td>
+	
+<table cellpadding="5" border=1 >
+		<th align="right">Pays</th>
+		<th align="right">Ecosyst&egrave;me</th>
+		<th align="right">Date d&eacute;but</th>
+		<th align="right">Date fin</th>	</th>
+	<tr align="right">
+		<td>The Gambia</td>
+		<td>Gambia River</td>
+		<td>O4/2001</td>
+		<td>05/2002</td>
 	</tr>
-	<tr>
+	<tr align="right">
 		<td>Mali</td>
 		<td>lac de Manantali</td>
-		<td>2002</td>
-		<td>2004</td>
+		<td>06/1995</td>
+		<td>08/1995</td>
+	</tr>
+	<tr align="right">
+		<td>Mali</td>
+		<td>lac de Manantali</td>
+		<td>05/2002</td>
+		<td>05/2003</td>
+	</tr>
+	<tr align="right">
+		<td>Mali</td>
+		<td>lac de Selingue</td>
+		<td>08/1994</td>
+		<td>10/1994</td>
+	</tr>
+	<tr align="right">
+		<td>Mali</td>
+		<td>lac de Selingue</td>
+		<td>05/2002</td>
+		<td>06/2003</td>
+	</tr>
+	<tr align="right">
+		<td>Senegal</td>
+		<td>Casamance</td>
+		<td>04/2005</td>
+		<td>08/2005</td>
 	</tr>
 </table>
+	
 <br>
 <br>
 
 <h2>P&ecirc;che scientifique</h2>
-
-
+<table cellpadding="5" border=1 >
+		<th align="right">Pays</th>
+		<th align="right">Ecosyst&egrave;me</th>
+		<th align="right">Campagnes</th>
+		<th align="right">Date d&eacute;but</th>
+		<th align="right">Date fin</th>	</th>
+	<tr align="right">
+		<td>The Gambia</td>
+		<td>Gambia River</td>
+		<td>13</td>
+		<td>24/11/2000</td>
+		<td>24/11/2003</td>
+	</tr>
+	<tr align="right">
+		<td>Guinea</td>
+		<td>Dangara</td>
+		<td>7</td>
+		<td>29/01/1993</td>
+		<td>24/01/1994</td>
+	</tr>
+	<tr align="right">
+		<td>Guinea</td>
+		<td>Fatala</td>
+		<td>13</td>
+		<td>22/01/1993</td>
+		<td>22/03/1994</td>
+	</tr>
+	<tr align="right">
+		<td>Guinea-Bissau</td>
+		<td>Bijagos</td>
+		<td>1</td>
+		<td>22/03/1993</td>
+		<td>01/04/1993</td>
+	</tr>
+	<tr align="right">
+		<td>Guinea-Bissau</td>
+		<td>Rio Buba</td>
+		<td>1</td>
+		<td>03/04/1993</td>
+		<td>07/04/1993</td>
+	</tr>
+	<tr align="right">
+		<td>Ivory Coast</td>
+		<td>Ebrié</td>
+		<td>73</td>
+		<td>17/12/1979</td>
+		<td>31/08/1982</td>
+	</tr>
+	<tr align="right">
+		<td>Mali</td>
+		<td>Manantali</td>
+		<td>3</td>
+		<td>19/06/2002</td>
+		<td>06/10/2003</td>
+	</tr>
+	<tr align="right">
+		<td>Mali</td>
+		<td>Selingue</td>
+		<td>3</td>
+		<td>10/06/2002</td>
+		<td>15/10/2003</td>
+	</tr>
+	<tr align="right">
+		<td>Mauritanie</td>
+		<td>Banc Arguin</td>
+		<td>3</td>
+		<td>07/05/2008</td>
+		<td>28/05/2010</td>
+	</tr>
+	<tr align="right">
+		<td>Senegal</td>
+		<td>Sine Saloum</td>
+		<td>63</td>
+		<td>20/04/1990</td>
+		<td>26/10/2007</td>
+	</tr>
+	<tr align="right">
+		<td>Senegal</td>
+		<td>Bamboung</td>
+		<td>25</td>
+		<td>11/03/2003</td>
+		<td>17/03/2011</td>
+	</tr>
+</table>
 
 	
 	</div> <!-- end div id="main_container"-->
